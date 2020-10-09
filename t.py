f90a95fd7c932ead0d014b70e54b7e7c7a9a478d
MrAbhaySharma <61902700+MrAbhaySharma@users.noreply.github.com>
2020-10-09 20:38:35 +0530

    Add files via upload

---
+++ t.py
@@ -0,0 +1,9 @@
+choice = raw_input(" What do you want to do? \n Enter:- \n \"add\" for addition \n \"sub\" for substraction \n \"multi\" for multiplication \n \"divide\" for division \n ")
+n1 = input("Enter two number (in this form \"2\"):- \n  ")
+n2 = input()
+if(choice=="add"):
+    s = n1 + n2
+    print("the sum is " + str(s))
+if(choice=="sub"):
+    r = n1 - n2
+    print("the diffrence is " + str(r))
