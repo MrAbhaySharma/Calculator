3aef38899f8743f09ebfb5384934e92066cc207a
mrabhaysharma <abhay.sharma20@outlook.com>
2020-10-10 09:29:32 +0530

    added 2 files.

---
+++ calculator.py
@@ -0,0 +1,9 @@
+while(True):
+    n1=input()
+    if(n1==0):
+        print("Enter 0 (\"Zero\") again to exit")
+        n2=raw_input()
+        if(n2=="0"):
+            print("Exiting...........................")
+            exit()
+        
--- t.py
+++ t.py
@@ -1,15 +1,23 @@
 choice = raw_input(" What do you want to do? \n Enter:- \n \"add\" for addition \n \"sub\" for substraction \n \"multi\" for multiplication \n \"divide\" for division \n ")
-n1 = input("Enter two number (in this form \"2\"):- \n  ")
-n2 = input()
+def input_number():
+    x1 = input("  Enter two number (in this form \"2\"):- \n  ")
+    x2 = input("  ")
+    return(x1,x2)
 if(choice=="add"):
+    n1,n2 = input_number()
     s = n1 + n2
-    print("the sum is " + str(s))
-if(choice=="sub"):
+    print("  the sum is " + str(s))
+elif(choice=="sub"):
+    n1,n2 = input_number()
     r = n1 - n2
-    print("the diffrence is " + str(r))
-if(choice=="multi"):
-    e = n1 + n2 
-    print("the product is " + str(e))
-if(choice=="divide"):
-    y = n1 + n2
-    print("the quotient is " + str(y))
+    print("  the diffrence is " + str(r))
+elif(choice=="multi"):
+    n1,n2 = input_number()
+    e = n1 * n2 
+    print("  the product is " + str(e))
+elif(choice=="divide"):
+    n1,n2 = input_number()
+    y = float(n1) / n2
+    print("  the quotient is " + str(y))
+else:
+    print("  Wrong Choice.")
